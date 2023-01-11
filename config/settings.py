b47ed36ef1cabb1a0d7c17719b375fdd733db50f
tomoki matsubuchi <matsubuchitmk@gmail.com>
2023-01-11 18:27:05 +0900

    secret key 隠蔽

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .settings_local import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h!fb56c8sh33$^zb2^^zv6yw5a10nj*d=@95bj3ylg#-tb*g=8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
